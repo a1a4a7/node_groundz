c35cb27a3271638728f1427132a21c9f9ae0643d
Man kit Ho <mankitho@Mans-MBP.lan>
2024-05-15 03:10:17 -0400

    retrigger

--- services/node-api/routes/userRoutes.js
+++ services/node-api/routes/userRoutes.js
@@ -6,4 +6,5 @@ const userController = require('../controllers/userControllers');
 router.get('/user', userController.getUser);
 router.post('/user', userController.createUser);
 
+// afb
 module.exports = router;
